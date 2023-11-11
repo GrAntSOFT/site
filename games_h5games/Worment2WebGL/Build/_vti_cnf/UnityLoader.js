70835c131eabd58f9793b20767531d73cb5f1b9e
GrAntSOFT <GrAntSOFT@yandex.ru>
2023-11-11 13:16:41 +0300

    nvu

---
+++ games_h5games/Worment2WebGL/Build/_vti_cnf/UnityLoader.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|25 Feb 2019 13:59:28 -0000
+vti_extenderversion:SR|12.0.0.6500
+vti_author:SR|NAFANYA\\QQ
+vti_modifiedby:SR|NAFANYA\\QQ
+vti_timecreated:TR|25 Feb 2019 13:59:28 -0000
+vti_cacheddtm:TX|25 Feb 2019 13:59:28 -0000
+vti_filesize:IR|155356
+vti_backlinkinfo:VX|games_h5games/Worment2WebGL/index.html
