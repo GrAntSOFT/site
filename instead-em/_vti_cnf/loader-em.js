8e2a8a6bb460cefd6d28cbe635fa8252f8ff36d3
GrAntSOFT <GrAntSOFT@yandex.ru>
2024-07-17 20:54:06 +0300

    Worment-3

---
+++ instead-em/_vti_cnf/loader-em.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|17 Feb 2024 20:09:11 -0000
+vti_extenderversion:SR|12.0.0.6500
+vti_author:SR|NAFANYA\\QQ
+vti_modifiedby:SR|NAFANYA\\QQ
+vti_timecreated:TR|17 Feb 2024 20:09:11 -0000
+vti_cacheddtm:TX|17 Feb 2024 20:09:11 -0000
+vti_filesize:IR|3677
+vti_backlinkinfo:VX|instead-em/instead-em.html
